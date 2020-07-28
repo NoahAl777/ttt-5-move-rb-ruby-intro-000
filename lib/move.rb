62b6fba789bcb4577297e6a910af3618e682c43e
Noah Alston <crashlive112@gmail.com>
2020-07-28 22:07:49 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 # code your input_to_index and move method here!
 def input_to_index(user_input)
 if {
-  (user_input).to_i =- 1 === user_input.to_i >= 0 && 
+  (user_input).to_i =- 1 === user_input.to_i >= 0 &&
   user_input.to_i <= 8;
   return user_input
 
